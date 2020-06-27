b382d15b5fbaa4fac7c21215e5afe3ac83cbfa60
tmakoba@bscglobal.com <tmakoba@bscglobal.com>
2020-06-27 14:30:51 +0200

    If ... Else

--- Program Flow/home.js
+++ Program Flow/home.js
@@ -15,7 +15,10 @@
 //     showMessage('true');
 // }
 
-if( +(1.1 + 1.3).toFixed(2) === 2.4 ){
-    let message = 'Hello';
-    showMessage(message);
+let price =28;
+
+if( price>10){    
+    showMessage('true');
+}else{
+    showMessage('false');
 }
