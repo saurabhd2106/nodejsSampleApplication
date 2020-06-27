d0d492ea30b1bd0b467150029908a4e0d3268676
tmakoba@bscglobal.com <tmakoba@bscglobal.com>
2020-06-27 13:42:30 +0200

    Types and Operators

--- Javascript Getting Started/Types and Operators/home.js
+++ Javascript Getting Started/Types and Operators/home.js
@@ -1,7 +1,51 @@
 
+// Numbers
+let _price = 12;
+// showMessage(price++);
+// showMessage(_price);
+// console.log(_price);
 
+// taxRate = 0.07;
+// price %= 5;
 
-let price = 25;
+// showMessage(price * taxRate);
+////////////////////////////////////////////////////////
 
-showMessage(typeof price);
-console.log(price);
+//Operator Precedence
+
+let price = 3+2*2;
+price = (3+2)*2;
+
+//Number Precision
+price = 1.1+1.3;
+
+//Negative numbers
+let amount = 20 - (-2);
+
+//strings
+let name = 'Andrea';
+let message =`Hello ${name}`;
+
+//Manipulating Strings
+message = 'Hello';
+message = message.length;
+
+//Converting Strings and Numbers
+amount = 123;
+amount = amount.toString();
+
+amount = Number.parseFloat("123.12");
+
+//Boolean Variable
+let saved_ = false;
+saved_ = !saved_;
+
+//Null and Undefined
+let saved;//undefined
+saved = null;
+
+//Objects and Symbols
+let person = {name:'Joe', Surname:'Doe'}
+
+showMessage(person.Surname);
+console.log(saved);
