570625b5ad578642c4504644307a3f1412600d76
tmakoba@bscglobal.com <tmakoba@bscglobal.com>
2020-06-27 14:25:43 +0200

    Truthy and Falsy

--- Program Flow/home.js
+++ Program Flow/home.js
@@ -1,4 +1,21 @@
-let price  = 20;
-if(price !== 15){
-    showMessage('discounted');
+
+// if("0"){
+//     showMessage('true');
+// }
+
+// if(""){
+//     showMessage('true');
+// }
+
+// if(0){
+//     showMessage('true');
+// }
+
+// if( +(1.1 + 1.3).toFixed(2) !== 2.4 ){
+//     showMessage('true');
+// }
+
+if( +(1.1 + 1.3).toFixed(2) === 2.4 ){
+    let message = 'Hello';
+    showMessage(message);
 }
