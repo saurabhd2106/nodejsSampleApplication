1c7a1ff6d9e87c74fa455b2484332e273c4424c6
tmakoba@bscglobal.com <tmakoba@bscglobal.com>
2020-06-28 02:45:52 +0200

    Showing and Hiding DOM Elements

--- Objects and the DOM/home.js
+++ Objects and the DOM/home.js
@@ -7,5 +7,14 @@ header.style.color = 'blue';
 const button = document.getElementById("see-review");
 
 button.addEventListener('click',function(){
-    console.log('click');
+    const review = document.getElementById('review');
+
+    if(review.classList.contains('d-none')){
+        review.classList.remove('d-none');
+        button.textContent ='CLOSE REVIEW';
+    }else{
+        review.classList.add('d-none');
+        button.textContent ='SEE REVIEW';
+    }  
+
 });
