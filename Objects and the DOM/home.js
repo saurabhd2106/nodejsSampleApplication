23caf91d60ef12bb6ed5dd12d8f16347f68c42f5
tmakoba@bscglobal.com <tmakoba@bscglobal.com>
2020-06-28 02:37:44 +0200

    Detecting button click

--- Objects and the DOM/home.js
+++ Objects and the DOM/home.js
@@ -2,5 +2,10 @@ const header = document.getElementById('message');
 
 header.style.fontWeight = '100';
 
-
 header.style.color = 'blue';
+
+const button = document.getElementById("see-review");
+
+button.addEventListener('click',function(){
+    console.log('click');
+});
